52831d78d85d5b780b37e05952e06df19eb49a46
LuismallitoDev <cardonaluisma@gmail.com>
2024-12-10 12:25:36 -0500

    Entrega

---
+++ src/main/java/zooAnimales/Ave.java
@@ -0,0 +1,65 @@
+package zooAnimales;
+import java.util.ArrayList;
+public class Ave extends Animal {
+    private static ArrayList<Ave> listado = new ArrayList<>();
+    private static int halcones;
+    private static int aguilas;
+    private String colorPlumas;
+
+    public Ave() {
+        listado.add(this);
+    }
+
+    public Ave(String nombre, int edad, String habitat, String genero, String colorPlumas) {
+        super(nombre, edad, habitat, genero);
+        this.colorPlumas = colorPlumas;
+        listado.add(this);
+    }
+
+    public static Ave crearHalcon(String nombre, int edad, String genero) {
+        halcones++;
+        return new Ave(nombre, edad, "montañas", genero, "café glorioso");
+    }
+
+    public static Ave crearAguila(String nombre, int edad, String genero) {
+        aguilas++;
+        return new Ave(nombre, edad, "selva", genero, "blanco y amarillo");
+    }
+
+    public static int cantidadAves() {
+        return listado.size();
+    }
+
+    // Getters y Setters
+    public String getColorPlumas() {
+        return colorPlumas;
+    }
+
+    public void setColorPlumas(String colorPlumas) {
+        this.colorPlumas = colorPlumas;
+    }
+
+    public static ArrayList<Ave> getListado() {
+        return listado;
+    }
+
+    public static void setListado(ArrayList<Ave> listado) {
+        Ave.listado = listado;
+    }
+
+    public static int getHalcones() {
+        return halcones;
+    }
+
+    public static void setHalcones(int halcones) {
+        Ave.halcones = halcones;
+    }
+
+    public static int getAguilas() {
+        return aguilas;
+    }
+
+    public static void setAguilas(int aguilas) {
+        Ave.aguilas = aguilas;
+    }
+}
